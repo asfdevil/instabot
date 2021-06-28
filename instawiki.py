c03b124d26cea4f3e6f28ec8e83bd650ba3c7f99
asfdevil <77090026+asfdevil@users.noreply.github.com>
2021-06-28 16:06:47 +0530

    Update instawiki.py

--- instawiki.py
+++ instawiki.py
@@ -1,8 +1,9 @@
 from instabot import Bot
 import os
 import wikipedia
-w = wikipedia.summary("about",sentences=10)
-c = os.system('rmdir /s config')
+y = ("enter subject")
+w = wikipedia.summary(y,sentences=10)
+c = os.system('rmdir /s config')#for windows
 b = input("enter your user name")
 a = input("enter your password")
 t = input("enter name of reciver")
